5dd85fef15b8137ec4c3048ee111cdd932a0886d
Dylan Lott <lott.dylan@gmail.com>
2026-01-17 16:27:56 -0700

    adds shutdown signal handling

--- app/app.go
+++ app/app.go
@@ -1,10 +1,16 @@
 package app
 
 import (
+	"context"
 	"database/sql"
 	"embed"
+	"errors"
 	"html/template"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 	_ "github.com/mattn/go-sqlite3"
@@ -56,5 +62,39 @@ func Run(templatesFS embed.FS) error {
 
 	r := buildRouter()
 	log.Info("Server listening on http://localhost:8080")
-	return http.ListenAndServe(":8080", r)
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	shutdownCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	serverErr := make(chan error, 1)
+	go func() {
+		serverErr <- srv.ListenAndServe()
+	}()
+
+	select {
+	case err := <-serverErr:
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			return err
+		}
+		return nil
+	case <-shutdownCtx.Done():
+		log.Info("Shutdown signal received")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		return err
+	}
+
+	err = <-serverErr
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+	return nil
 }
